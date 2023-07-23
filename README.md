b93fe1e8516d9e85842b60452bb9fd6dd7c92b0b
biFebriansyah <bukanebi@gmail.com>
2023-07-23 16:22:52 +0700

    change repos

---
+++ README.md
@@ -0,0 +1,21 @@
+# FazzBackup Website
+
+https://bifebriansyah.github.io/fazzbackupweb/
+
+## Development
+
+```bash
+yarn install
+```
+
+### Start Dev server
+
+```bash
+yarn start
+```
+
+## Publish pages
+
+```bash
+pnpm build
+```
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 style={{ marginBottom: '20px' }}>fazzbackup</h1>
+                <code class="lead">
+                    <span>Designed and built by&nbsp;</span>
+                    <a href="https://github.com/gobackup/gobackup">gobackup/gobackup</a>
+                    <span>&nbsp;and modified for personal needs.</span>
+                </code>
+            </header>
+        </div>
+    )
+}
+
+export default App
